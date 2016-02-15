784617607f5777d67bcfd068955650e4c73f07d7
Craig Derington <craig@craigderington.me>
2016-02-15 13:07:38 -0500

    Update models.py

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
+# define the models
 
 class HomePage(Page):
     body = RichTextField(blank=True)
