bf0b88be3e526e922cb2f743e4ccca3f6f15f0fb
Craig Derington <craig@craigderington.me>
2016-02-15 16:34:33 -0500

    Update Home models.py

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
@@ -9,7 +10,10 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     body = RichTextField(blank=True)
-    
+
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full")
     ]
+
+    class Meta:
+        verbose_name = 'homepage'
